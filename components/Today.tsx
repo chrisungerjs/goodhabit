21d13bc32961464bd9be86dd90576d55079daff8
christian <ceunger91@gmail.com>
2020-11-14 20:24:57 -0500

    trying conditional render in today component

--- components/Today.tsx
+++ components/Today.tsx
@@ -17,7 +17,9 @@ import HabitCard from './HabitCard'
 
 const Today: React.FC = () => {
   const [updateHabit] = useUpdateHabitMutation()
-  const { data } = useGetHabitsQuery()
+  const { error, loading, data } = useGetHabitsQuery()
+  if (loading) return <>Loading...</>
+  if (error) return <>Error: {error}</>
   const [updateHistory] = useUpdateHistoryMutation()
   const [orderMap, setOrderMap] = useState(data.habits?.map((habit) => habit._id))
   const todayDate = archiveDate(new Date())
