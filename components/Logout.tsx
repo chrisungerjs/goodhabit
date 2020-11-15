f69649187e47be4e87e367a4e52c60b210dd6728
christian <ceunger91@gmail.com>
2020-11-14 20:51:34 -0500

    testing cache reset

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -2,9 +2,9 @@ import { useContext } from 'react'
 import { Button } from 'react-bootstrap'
 import Cotter from 'cotter'
 import { Context } from '../util/context'
+import { initializeApollo } from '../util/apolloClient'
 
-const Logout: React.FC = (props) => {
-  console.log(props)
+const Logout: React.FC = () => {
   const { state, dispatch } = useContext(Context)
   const cotter = new Cotter('ca212de7-300a-4354-a178-24f474b3ae69')
   return (
@@ -21,6 +21,7 @@ const Logout: React.FC = (props) => {
           dispatch({
             type: "LOGGED_OUT_USER"
           })
+          initializeApollo(null)
         }}
       >
         Log out {state.user.identifier}
